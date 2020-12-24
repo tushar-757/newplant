f867e6400c347cca832b7359e219b40a8fe28a7d
abc <abc@example.com>
2020-12-24 13:47:58 +0530

    removal of socket.io

--- src/Dashboard/containers/DashboardContainer.js
+++ src/Dashboard/containers/DashboardContainer.js
@@ -1,6 +1,5 @@
  import Dashboard from '../components/Dashboard';
  import React,{useEffect} from 'react';
- import Socketio from 'socket.io-client';
  import {withRouter} from 'react-router-dom';
  function DashboardContainer(props) {
             useEffect(()=>{
@@ -9,11 +8,7 @@
                 }
             }
             , [props.location.pathname]);
-            useEffect(()=>{
-                  const socket=Socketio("https://fathomless-fortress-06365.herokuapp.com/",{ transport : ['websocket'] })
-                  socket.emit('chat message', "hello");
-                  socket.on("message",(msg)=>{console.log(msg)})
-            },[])
+         
             return (
                <>
                <Dashboard/>
