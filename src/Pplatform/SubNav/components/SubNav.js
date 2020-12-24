38c6a6bc6dc5e529583094a244cf8ef17b24d1d4
abc <abc@example.com>
2020-12-24 16:56:33 +0530

    added

--- src/Pplatform/SubNav/components/SubNav.js
+++ src/Pplatform/SubNav/components/SubNav.js
@@ -1,10 +1,116 @@
 import React from 'react';
 import '../SCSS/styles.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import {NavLink,Link,withRouter} from 'react-router-dom';
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    toolbar: {
+      minHeight: 128,
+      alignItems: 'flex-start',
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      alignSelf: 'flex-end',
+    },
+  }));
 function SubNav(props) {
+    const classes = useStyles();
     return (
-   <div className="subnav-container">
-        <NavLink to="/"
+        <>
+     {(props.wid<650)?
+        <div className="nav-head">
+            {!props.toggle?<button className="nav-btn"onClick={()=>props.togglefunc()}>:</button>:
+            <>
+            <button className="nav-btn" onClick={()=>props.togglefunc1()}>x</button>
+              <div className="nav-head-cont">
+                    <NavLink to="/"
+            activeClassName="subnav-common  options-selected"
+            className= "subnav-common"
+            style={{
+                textDecoration: "none"
+            }}>
+                <div className="subnav-header">GreenOplants</div>
+            </NavLink>
+       <div className="platform">
+            Platform
+       </div>
+        <div className="subnav-options">
+           <NavLink
+                to="/platform/Plants"
+                className={props.location.pathname.includes("/platform/Plants") ? "subnav-common  options-selected" : "subnav-common"}
+                style={{
+                    textDecoration: "none"
+                }}>
+               <span  onClick={()=>props.togglefunc1()}>
+                   Plants
+               </span>
+                </NavLink>    
+               <Link
+                to="/platform/Tools"
+                className={props.location.pathname.includes("/platform/Tools") ? "subnav-common  options-selected" : "subnav-common"}
+                style={{
+                    textDecoration: "none"
+                }}>
+               <div>
+                   <span  onClick={()=>props.togglefunc1()}>
+                       Tools
+                    </span>
+               </div>
+                </Link>
+             <NavLink
+                to="/platform/Stats"
+                activeClassName= "subnav-common  options-selected"
+                className= "subnav-common"
+                style={{
+                    textDecoration: "none"
+                }}>
+               <div>
+                   <span  onClick={()=>props.togglefunc1()}>  
+                     Stats
+                    </span> 
+               </div>
+              </NavLink>
+              <Link
+                to="/platform/Reviews"
+                className={props.location.pathname.includes("/platform/Reviews") ? "subnav-common  options-selected" : "subnav-common"}
+                style={{
+                    textDecoration: "none"
+                }}>
+               <div>
+                   <span  onClick={()=>props.togglefunc1()}>
+                       Reviews
+                    </span>
+               </div>
+                </Link>
+    
+              <div className="Sub-nav-login" onClick={()=>props.history.push("/platform/Login")}>
+               <span  onClick={()=>props.togglefunc1()}>  
+                 Login
+                </span> 
+              </div>
+          <div  className="Sub-nav-signup" onClick={()=>props.history.push("/platform/Signup")}>
+              <span  onClick={()=>props.togglefunc1()}>
+                  SignUp
+              </span>
+          </div>
+             </div>
+              </div>
+              </>
+            }
+         </div>:
+           <div className="subnav-container">
+           <>
+             <NavLink to="/"
             activeClassName="subnav-common  options-selected"
             className= "subnav-common"
             style={{
@@ -74,8 +180,8 @@ function SubNav(props) {
                   SignUp
               </span>
           </div>
-             </div>
-             </div>
+             </div></>
+             </div>}</>
     )}
 
 export default withRouter(SubNav);
--- src/Pplatform/SubNav/containers/SubNavContainer.js
+++ src/Pplatform/SubNav/containers/SubNavContainer.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useLayoutEffect,useState,useEffect } from "react";
 import SubNav from "../components/SubNav";
 import { Route, withRouter } from "react-router-dom";
 import Header from '../../Header';
@@ -17,6 +17,8 @@ function RouteWithSubRoutes({ route }) {
 }
 
 function SubNavContainer(props) {
+  const [slide,setslide]=useState();
+  const [toggle,settoggle]=useState(false);
   useEffect(() => {
     if (props.location.pathname === "/platform") {
         props.history.push("/platform/dashboard");
@@ -24,9 +26,24 @@ function SubNavContainer(props) {
         props.history.push("/platform/Plants/Indoor");
     }
   }, [props.history.location]);
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setslide(window.innerWidth);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  const handletoggle=()=>{
+    settoggle(true)
+  }
+  const handletoggle1=()=>{
+    settoggle(false)
+  }
   return (
     <Fragment>
-        <SubNav /> 
+        <SubNav wid={slide} togglefunc={handletoggle} toggle={toggle} togglefunc1={handletoggle1}/> 
         <Header/>
         {routes.map((route, i) => {
           return <RouteWithSubRoutes key={i} route={route} />;
