34e148bf994ea18866523a244207923aa11fdc03
Tushar Vashistha <63191672+tushar-757@users.noreply.github.com>
2021-08-03 18:08:43 +0530

    Update index.js
    changed with add

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <HashRouter>
+    <script data-ad-client="ca-pub-7605684511525277" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <App />
   </HashRouter>,
   document.getElementById('root')
